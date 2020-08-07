13a083ee12ebbca640f568a0bcbb8fc05844ef86
VimalKShah <vshah925@gmail.com>
2020-08-07 12:31:40 +0530

    MVPP Pattern Example.

---
+++ MVPPPatternExample/app/src/main/java/com/example/mvpppatternexample/LoginViewModel.java
@@ -0,0 +1,18 @@
+package com.example.mvpppatternexample;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+
+    MutableLiveData<String> status = new MutableLiveData<>();
+
+    void doLogin(String username, String password) {
+        UserModel userModel = new UserModel(username, password);
+        String msg = "Login failed";
+        if(userModel.checkValidity()) {
+            msg = "Login Successful";
+        }
+        status.postValue(msg);
+    }
+}
